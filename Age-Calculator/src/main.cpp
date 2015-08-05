8bb89cf7f30709d50daaf0c6a1d1702dfefc9647
jdm1891 <jordan1891189118911891@gmail.com>
2015-08-06 00:44:00 +0100

    Update main.cpp
    I made it a lot more accurate

--- Age-Calculator/src/main.cpp
+++ Age-Calculator/src/main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 double e = 2.71828182845904523536;
 
-double gemAge, humanAge, dev, accRange, acc;
+double gemAge, humanAge, gemAgeOld, dev, accRange, acc;
+bool average = false;
 
 int doHuman(double humanAge)
 {
@@ -19,18 +20,31 @@ int doHuman(double humanAge)
     for(double x = 1; x <=100000/accRange;x++) //total age is x/100 so right now it's 100,000
     {
         double y = (pow(e, -0.000210*(x*accRange))*20); //took a a very long time to realise that it's e^ect ect*20 and not e*20^ect ect
-        if(y >= dev-accRange && y <= dev+accRange) //the lower the o.o1 number is, the more accurate it is, but make s8re to make x bigger too
+        if(y >= dev-0.01 && y <= dev+0.01) //the lower the o.o1 number is, the more accurate it is, but make s8re to make x bigger too
         {
             gemAge = x*accRange;
-            cout << "\nHuman Age: " << humanAge << "\nMinimum Gem Age: " << gemAge*accRange << endl;
-            system("pause");
-            return 1;
+            cout << "\nHuman Age: " << humanAge << "\nMinimum Gem Age: " << gemAge << endl;
+         //   cout << dev << endl << y;
+            if(!average)
+            {
+                 gemAgeOld = gemAge;
+                 average = true;
+            }
+
         }
         else
         {
             cout << "nothing found for " << x*accRange << endl;
+            gemAgeOld = gemAge-1;
+            if(gemAgeOld>0)
+            {
+                Sleep(500);
+                system("cls");
+                cout << "\nHuman Age: " << humanAge << "\nEstimated Gem Age: " << (gemAge+gemAgeOld)/2 << endl;
+                system("pause");
+                return 0;
+            }
         }
-
     }
 }
 
@@ -52,8 +66,6 @@ int getAcc()
     {
         accRange=accRange/10;
     }
-    cout << accRange;
-    system("pause");
 }
 
 int getAge()
@@ -61,13 +73,14 @@ int getAge()
     system("cls");
     cout << "Enter age to convert to gem:";
     cin >> humanAge;
+    return humanAge;
 }
+
 
 
 int main()
 {
     getAcc();
-    getAge();
-    doHuman(humanAge);
+    doHuman(getAge());
     return 0;
 }
